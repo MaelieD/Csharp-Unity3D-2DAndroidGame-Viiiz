da97ae1a7bd47ca2ae5640d799ef0c7e46e9900b
MaelieD <dessapt@et.esiea.fr>
2016-11-28 10:34:29 +0100

    - NEW : first physical assets and first tests for character behaviour

---
+++ Viiiz/Assets/2_scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	[SerializeField]
+	private Rigidbody2D rgbd;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rgbd.velocity = new Vector3 (0, 50, 0);
+
+		if(Input.GetKey(KeyCode.Space))
+			rgbd.velocity = new Vector3 (60, 50, 0);
+
+		/*float moveHorizontal = Input.GetAxis ("Horizontal"); // left and right
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.2f);
+		rgbd.AddForce (movement * speed);//*/
+	}
+}
---
+++ Viiiz/Assets/2_scripts/WallGenerator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallGenerator : MonoBehaviour {
+
+	// Color Level 1
+	Color c1 = new Color(178, 39, 0, 255);//#B22700;
+	Color c2 = new Color(255, 76, 25, 255);//#FF4C19;
+	Color c3 = new Color(255, 56, 0, 255);//#FF3800;
+
+	public int maxPlatforms = 50;
+
+	[SerializeField]
+	public GameObject squareWallRight;
+	[SerializeField]
+	public GameObject squareWallLeft;
+
+	public int horizontalMin = 1;
+	public int horizontalMax = 5;
+
+	private Vector2 originPosition;
+
+	// Use this for initialization
+	void Start () {
+		SpawnSquares (squareWallRight.transform.position);
+		SpawnSquares (squareWallLeft.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void SpawnSquares(Vector3 originPosition)
+	{
+		for (int i = 0; i < maxPlatforms; i++) {
+			Vector3 nextPosition = originPosition;
+			for (int j = 0; j < Random.Range(horizontalMin, horizontalMax); j++)
+			{
+				Instantiate (squareWallRight, nextPosition, Quaternion.identity);
+				originPosition = nextPosition;
+			}
+		}
+	}
+}
